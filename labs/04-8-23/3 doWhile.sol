eabc41a44efdb4b2ca60db98969fa79f57816fa8
majeed-abdul <majeedabdul.941485@gmail.com>
2023-08-07 15:33:40 +0500

    labs 4 partial

--- labs/04-8-23/3 doWhile.sol
+++ labs/04-8-23/3 doWhile.sol
@@ -5,12 +5,11 @@ contract Loop {
     uint256[] public arr;
     function ODDNum(uint256 max) public {
         uint256 i = 1;
-        while (i <= max) {
+        do {
             if (i % 2 != 0) arr.push(i);
             i++;
-        }
+        }while(i<=max);
     }
-
     function get() public view returns (uint256[] memory) {
         return arr;
     }
